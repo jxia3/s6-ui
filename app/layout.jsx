6e5ae6a1a579451bd937a7d8a82449e046918c27
jxia3 <97870693+jxia3@users.noreply.github.com>
2023-07-11 16:26:32 -0700

    add footer

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,6 +12,15 @@ const RootLayout = ({ children }) => (
         <body className={styles.center}>
             <div className={styles.content}>
                 <StyledJsxRegistry>{children}</StyledJsxRegistry>
+                <div className={styles.footer}>
+                    Research by&nbsp;
+                    <a className={styles["footer-link"]} href="https://cs.brown.edu/~spr/research/s6.html" target="_blank">Steven P. Reiss</a>,
+                    frontend by&nbsp;
+                    <a className={styles["footer-link"]} href="https://github.com/jxia3" target="_blank">Jerry Xia</a>
+                    <a className={styles["repo-link"]} href="https://github.com/jxia3/s6-ui" target="_blank">
+                        <img className={styles["repo-icon"]} src="/github.svg"></img>
+                    </a>
+                </div>
             </div>
         </body>
     </html>
