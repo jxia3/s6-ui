4f975e55e91a77892a7a45dcd6d292521feddd21
jxia3 <97870693+jxia3@users.noreply.github.com>
2023-07-19 11:17:59 -0700

    refactor method search

---
+++ app/search/method/details.jsx
@@ -0,0 +1,98 @@
+import TextInput from "./input.jsx"
+import Tests from "./tests.jsx"
+
+// Search details card
+
+const SearchBox = ({
+    description,
+    setDescription,
+    descriptionError,
+    setDescriptionError,
+    declaration,
+    setDeclaration,
+    declarationError,
+    setDeclarationError,
+    setMethod,
+    tests,
+    setTests,
+    search,
+}) => {
+    // Get function signature data from server on blur
+
+    async function checkSignature() {
+        try {
+            const check = await fetch("/api/method/signature", {
+                method: "POST",
+                body: JSON.stringify({
+                    signature: declaration,
+                }),
+            }).then(response => response.json())
+            
+            if (check?.error || check?.PROBLEM) {
+                setDeclarationError(check?.error || check?.PROBLEM)
+            } else if (check?.RESULT?.SIGNATURE?.METHOD) {
+                setMethod(check.RESULT.SIGNATURE.METHOD)
+                setDeclaration(check.RESULT.SIGNATURE.METHOD.TEXT)
+            } else {
+                console.error(new Error("Unable to interpret server response " + JSON.stringify(check)))
+            }
+        } catch(error) {
+            console.error(error)
+        }
+    }
+
+    return (
+        <>
+            <div className="card">
+                <h2 className="title">Method Search</h2>
+                <TextInput
+                    label="Description (keywords)"
+                    value={description}
+                    setValue={setDescription}
+                    error={descriptionError}
+                    setError={setDescriptionError}
+                />
+                <TextInput
+                    label="Declaration (signature)"
+                    value={declaration}
+                    setValue={setDeclaration}
+                    error={declarationError}
+                    setError={setDeclarationError}
+                    onChange={() => setMethod(null)}
+                    onBlur={checkSignature}
+                    monospace
+                />
+                <Tests tests={tests} setTests={setTests} />
+                <button className="search" onClick={search}>SEARCH</button>
+            </div>
+            <style jsx>{`
+                .card {
+                    width: min(750px, 100%);
+                    background-color: var(--fill);
+                    border-radius: 4px;
+                    padding: 30px 40px;
+                    margin-bottom: 30px;
+                }
+
+                .title {
+                    font-size: 1.5rem;
+                    font-weight: normal;
+                    margin-bottom: 20px;
+                }
+
+                .search {
+                    font-size: 1.2rem;
+                    background-color: var(--color-dark);
+                    padding: 0.6rem 1.2rem;
+                    margin-top: 5px;
+                }
+
+                .search:hover {
+                    background-color: var(--color-extra-dark);
+                }
+            `}</style>
+        </>
+    )
+}
+
+export { SearchBox }
--- app/search/method/search.jsx
+++ app/search/method/search.jsx
@@ -1,9 +1,8 @@
-import TextInput from "./input.jsx"
-import Tests from "./tests.jsx"
+import { SearchBox } from "./details.jsx"
 import SearchResults, { SearchState } from "../results.jsx"
 import { useState } from "react"
 
-// Method search card
+// Method search layout
 
 const MethodSearch = () => {
     const [ searchState, setSearchState ] = useState(SearchState.NONE)
@@ -19,30 +18,6 @@ const MethodSearch = () => {
         right: "",
         error: null,
     }])
-
-    // Get function signature data from server on blur
-
-    async function checkSignature() {
-        try {
-            const check = await fetch("/api/method/signature", {
-                method: "POST",
-                body: JSON.stringify({
-                    signature: declaration,
-                }),
-            }).then(response => response.json())
-            
-            if (check?.error || check?.PROBLEM) {
-                setDeclarationError(check?.error || check?.PROBLEM)
-            } else if (check?.RESULT?.SIGNATURE?.METHOD) {
-                setMethod(check.RESULT.SIGNATURE.METHOD)
-                setDeclaration(check.RESULT.SIGNATURE.METHOD.TEXT)
-            } else {
-                console.error(new Error("Unable to interpret server response " + JSON.stringify(check)))
-            }
-        } catch(error) {
-            console.error(error)
-        }
-    }
 
     // Search for method
 
@@ -181,55 +156,21 @@ const MethodSearch = () => {
 
     return (
         <>
-            <div className="card">
-                <h2 className="title">Method Search</h2>
-                <TextInput
-                    label="Description (keywords)"
-                    value={description}
-                    setValue={setDescription}
-                    error={descriptionError}
-                    setError={setDescriptionError}
-                />
-                <TextInput
-                    label="Declaration (signature)"
-                    value={declaration}
-                    setValue={setDeclaration}
-                    error={declarationError}
-                    setError={setDeclarationError}
-                    onChange={() => setMethod(null)}
-                    onBlur={checkSignature}
-                    monospace
-                />
-                <Tests tests={tests} setTests={setTests} />
-                <button className="search" onClick={search}>SEARCH</button>
-            </div>
+            <SearchBox
+                description={description}
+                setDescription={setDescription}
+                descriptionError={descriptionError}
+                setDescriptionError={setDescriptionError}
+                declaration={declaration}
+                setDeclaration={setDeclaration}
+                declarationError={declarationError}
+                setDeclarationError={setDeclarationError}
+                setMethod={setMethod}
+                tests={tests}
+                setTests={setTests}
+                search={search}
+            />
             <SearchResults searchState={searchState} result={result} />
-            <style jsx>{`
-                .card {
-                    width: min(800px, 100%);
-                    background-color: var(--fill);
-                    border-radius: 4px;
-                    padding: 30px 50px;
-                    margin-bottom: 30px;
-                }
-
-                .title {
-                    font-size: 1.5rem;
-                    font-weight: normal;
-                    margin-bottom: 20px;
-                }
-
-                .search {
-                    font-size: 1.2rem;
-                    background-color: var(--color-dark);
-                    padding: 0.6rem 1.2rem;
-                    margin-top: 5px;
-                }
-
-                .search:hover {
-                    background-color: var(--color-extra-dark);
-                }
-            `}</style>
         </>
     )
 }
